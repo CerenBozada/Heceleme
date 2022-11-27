5c3277523c00f73a89ad63643a029a08f76e5948
Ceren Bozada <69468105+CerenBozada@users.noreply.github.com>
2022-11-27 12:08:51 +0300

    Add files via upload

---
+++ hecele/src/hecele.js
@@ -0,0 +1,60 @@
+
+const unluler = ["a", "e", "ı", "i", "o", "ö", "u", "ü"]
+
+const isV = x => unluler.includes(x)
+
+const vowelMask = text => text.split("").map((char, pos) => isV(char))
+
+const splitBy = (text, index) => [text.slice(0, index), text.slice(index)]
+
+export const hecele = text => {
+    text = text.toLowerCase()
+    let heceler = []
+
+    while (true) {
+        const positions = vowelMask(text)
+
+        if (positions.length <= 1) {
+            if (text !== "") {
+                heceler.push(text)
+            }
+            break
+        }
+
+        const pos = positions.indexOf(true)
+
+        try {
+            let left = []
+            if (isV(text[pos + 1])) { // loanwords 
+                if (text[pos] === text[pos + 1]) { //saadet müsaade
+                    left = splitBy(text, pos + 2)
+                } else { //müsait şair 
+                    left = splitBy(text, pos + 1)
+                }
+            } else if (isV(text[pos + 2])) {
+                left = splitBy(text, pos + 1)
+            } else if (isV(text[pos + 3])) {
+                left = splitBy(text, pos + 2)
+            } else if (text.slice(pos + 1, pos + 4) in ["str", "ktr", "ctr", "ntr"]) { //elektronik #kontrol loanwords
+                left = splitBy(text, pos + 2)
+            } else {
+                left = splitBy(text, pos + 3)
+            }
+
+            heceler.push(left[0])
+            text = left[1]
+
+        } catch {
+
+            break
+        }
+    }
+
+    return heceler
+}
+
+
+
+
+
+
